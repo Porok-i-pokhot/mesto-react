988ed0ab83fc7c3defeacc7ecf8b2ca6591b4ffa
Natalia Ustimenko <natalia.ustimenko93@gmail.com>
2020-10-09 11:46:17 -0700

    fixed code for displaying likes on cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Card({name, likes, link}) {
+    debugger;
     return(
             <div className="element">
                 <button className="element__delete defocus"/>
@@ -9,7 +10,7 @@ function Card({name, likes, link}) {
                     <h2 className="element__title">{name}</h2>
                     <div className="element__like-container">
                         <button type="button" className="element__like defocus"/>
-                        <p className="element__likes-quantity">{likes}</p>
+                        <p className="element__likes-quantity">{likes.length}</p>
                     </div>
                 </div>
             </div>
--- src/utils/api.js
+++ src/utils/api.js
@@ -5,64 +5,64 @@ export default class Api {
   }
 
   getInitialCards() {
-      return Promise.resolve([
-          {
-              name: 'Алтайский край',
-              link: 'https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80'
-          },
-          {
-              name: 'Ладожское озеро',
-              link: 'https://images.unsplash.com/photo-1547846218-c982107d30f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1972&q=80'
-          },
-          {
-              name: 'Гора Нургуш',
-              link: 'https://images.unsplash.com/photo-1506516493400-bb5e8347bf0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80'
-          },
-          {
-              name: 'Судак, Крым',
-              link: 'https://images.unsplash.com/photo-1565342403875-07a8dc5ed13c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
-          },
-          {
-              name: 'Байкал',
-              link: 'https://images.unsplash.com/photo-1551844931-cfcfecb3636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2034&q=80'
-          },
-          {
-              name: 'Владивосток',
-              link: 'https://images.unsplash.com/photo-1563943078-d83d3fb86468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
-          }
-      ]);
+      // return Promise.resolve([
+      //     {
+      //         name: 'Алтайский край',
+      //         link: 'https://images.unsplash.com/photo-1494791286225-ea86fc957ba7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80'
+      //     },
+      //     {
+      //         name: 'Ладожское озеро',
+      //         link: 'https://images.unsplash.com/photo-1547846218-c982107d30f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1972&q=80'
+      //     },
+      //     {
+      //         name: 'Гора Нургуш',
+      //         link: 'https://images.unsplash.com/photo-1506516493400-bb5e8347bf0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1955&q=80'
+      //     },
+      //     {
+      //         name: 'Судак, Крым',
+      //         link: 'https://images.unsplash.com/photo-1565342403875-07a8dc5ed13c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80'
+      //     },
+      //     {
+      //         name: 'Байкал',
+      //         link: 'https://images.unsplash.com/photo-1551844931-cfcfecb3636f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2034&q=80'
+      //     },
+      //     {
+      //         name: 'Владивосток',
+      //         link: 'https://images.unsplash.com/photo-1563943078-d83d3fb86468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
+      //     }
+      // ]);
 
-    // return fetch(`${this.baseUrl}/cards`, {
-    //   method: 'GET',
-    //   headers: this.headers
-    // })
-    //   .then((res) => {
-    //     if(res.ok) {
-    //       return res.json();
-    //     }
-    //     return Promise.reject(`Ошибка: ${res.status}`);
-    //   });
+    return fetch(`${this.baseUrl}/cards`, {
+      method: 'GET',
+      headers: this.headers
+    })
+      .then((res) => {
+        if(res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
   }
 
   getUserInfo() {
-      return Promise.resolve({
-          "name": "Jacques Cousteau",
-          "about": "Sailor, researcher",
-          "avatar": "https://inc-news.ru/data/inc/preview/2019-12/16/image-6578-1576445378-620x886.jpg",
-          "_id": "e20537ed11237f86bbb20ccb",
-          "cohort": "cohort0"
-      });
+      // return Promise.resolve({
+      //     "name": "Jacques Cousteau",
+      //     "about": "Sailor, researcher",
+      //     "avatar": "https://inc-news.ru/data/inc/preview/2019-12/16/image-6578-1576445378-620x886.jpg",
+      //     "_id": "e20537ed11237f86bbb20ccb",
+      //     "cohort": "cohort0"
+      // });
 
-    // return fetch(`${this.baseUrl}/users/me`, {
-    //   method: 'GET',
-    //   headers: this.headers
-    // })
-    //   .then((res) => {
-    //     if(res.ok) {
-    //       return res.json();
-    //     }
-    //     return Promise.reject(`Ошибка: ${res.status}`);
-    //   });
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this.headers
+    })
+      .then((res) => {
+        if(res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
   }
 
   setEditedUserInfo(data) {
@@ -160,7 +160,7 @@ export default class Api {
 export const api = new Api({
     baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-15',
     headers: {
-        authorization: 'dba46c0c-2940-4d7d-b159-f4b22b002c4d',
+        authorization: 'c4aab964-a0d5-47da-a56b-0a1454d3e9d4',
         'Content-Type': 'application/json'
     }
 });
